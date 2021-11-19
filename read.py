90ded26b34b2ffd5c805ef95b52666ecd05f62e2
Vivi Chung <vivi31527@hotmail.com.tw>
2021-11-19 20:50:28 +0800

    add file

---
+++ read.py
@@ -0,0 +1,14 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 100000 == 0:
+			print(len(data))
+print('檔案讀取完了 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每一筆留言的平均長度是', sum_len/len(data))
