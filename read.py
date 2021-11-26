1773a866f7e55f3365e5164e9495ebb592304ad2
Vivi Chung <vivi31527@hotmail.com.tw>
2021-11-26 11:39:02 +0800

    add filter

--- read.py
+++ read.py
@@ -6,9 +6,21 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 100000 == 0:
 			print(len(data))
-print('檔案讀取完了 總共有', len(data), '筆資料')
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('每一筆留言的平均長度是', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100字')
+
+GOOD = []
+for d in data:
+	if 'good' in d :
+		GOOD.append(d)
+print('一共有', len(GOOD), '筆留言內容提到good')
